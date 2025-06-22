dd38448b25539cd4d6566a7cdfbf9d955f472788
AnikLeo <anikeevleonid@gmail.com>
2025-06-22 09:06:03 +0300

    Update Py111_lab2_2.py

--- Py111_lab2_2.py
+++ Py111_lab2_2.py
@@ -7,6 +7,10 @@ def factorial_iterative(n: int) -> int:
     """
     if n < 0:
         raise ValueError("Факториал отрицательного цисла не может быть найден")
+    if not isinstance(n, int):
+        raise TypeError("Только целое число")
+
+    
     fact_n = 1
     for i in range(1, n+1):
         fact_n *= i
